c1161d010b930089d2285b910f1baca9fb0b8a5a
DaviFSilva <93409413+DaviFSilva@users.noreply.github.com>
2024-01-30 15:58:24 -0300

    hotfiz missing bracket

--- magicscouting/src/routes/qrCodeScanner/+page.svelte
+++ magicscouting/src/routes/qrCodeScanner/+page.svelte
@@ -92,7 +92,7 @@
 <Modal bind:showModal={showModal} showX={false}>
     <h2 class="mb-2 text-2xl">Armazenar Dados?</h2>
     <div class="box-border flex flex-row justify-between w-full">
-        <button on:click={HandleStore; showModal = false} class="w-[45%] p-2 shadow-md shadow-black bg-white active:shadow-inner text-black rounded-md">Sim</button>
+        <button on:click={() => {HandleStore(); showModal = false}} class="w-[45%] p-2 shadow-md shadow-black bg-white active:shadow-inner text-black rounded-md">Sim</button>
         <button on:click={() => {showModal = false; console.log(showModal)}} class="w-[45%] p-2 shadow-md shadow-black bg-white text-black rounded-md active:shadow-inner">Não</button>
     </div>
 </Modal>
