7f7806a025f44dc349bf979f8bc5238ea3ba6af3
DaviFSilva <93409413+DaviFSilva@users.noreply.github.com>
2024-02-01 23:56:50 -0300

    :globe_with_meridians: feat(i18n): apply i18n to about page

--- magicscouting/src/routes/about/+layout.svelte
+++ magicscouting/src/routes/about/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
-
+	import { goto } from "$app/navigation";
+	import { _ } from "svelte-i18n";
 </script>
 
 <div class="flex flex-row items-start justify-center p-6 text-neutral-600 dark:text-white app">
 	<main class="flex flex-col items-center justify-center">
-		<h1 class="text-4xl text-primary-heavy dark:text-white">Sobre o projeto</h1>
+		<i class="absolute z-10 text-2xl text-white top-4 left-6 fa-solid fa-angle-left" on:click={() => {goto('/')}}></i>
+		<h1 class="text-4xl text-primary-heavy dark:text-white">{$_('about.title')}</h1>
 
 		<div class="w-[30vw] separator mb-1"></div>
 		<slot />
--- magicscouting/src/routes/about/+page.svelte
+++ magicscouting/src/routes/about/+page.svelte
@@ -1,19 +1,19 @@
 <script>
-    
+    import { _ } from "svelte-i18n";
 </script>
-<div class="w-full text-left text-justify">
-  <p>O projeto de um app de scouting surgiu no início do ano de 2023, quando um membro da equipe de programação encontrou um repositório de um aplicativo de scouting, simples porém eficaz. Como o aplicativo era reservado para o jogo de 2022, tivemos que fazer algumas alterações para a nova temporada. Depois de um ano usando esse projeto inicial, decidimos dar um passo em frente, e fazer um aplicativo próprio da equipe.</p>
+<div class="w-full text-justify">
+  <p>{$_('about.paragraph_1')}</p>
 </div>
-<h1 class="my-3 text-3xl">Intuito do projeto</h1>
-<div class="w-full text-left text-justify">
-  <p>O projeto do app MagicScouting surgiu da necessidade de um app mais capacitado que seu antecessor. A equipe que trabalhou no projeto antigo queria fazer algo que tivesse 100% de autoria da equipe, e assim se fez. Além disso, enxergamos nesse projeto a oportunidade de ajudar outras equipes durante a competição, essa visão se tornou a nossa meta.</p>
+<h1 class="my-3 text-3xl">{$_('about.purpose_title')}</h1>
+<div class="w-full text-justify">
+  <p>{$_('about.paragraph_2')}</p>
 </div>
-<h1 class="my-3 text-3xl">Criadores</h1>
+<h1 class="my-3 text-3xl">{$_('about.creators.title')}</h1>
 <div class="flex items-start w-full flox-col">
   <ul class="list-disc list-outside">
-    <li>Franco Valois De Luca</li>
-    <li>Gabrielli Fertig de Oliveira</li>
-    <li>Davi Feitosa Silva Santana</li>
-    <li>Helena Scussel Rosa</li>
+    <li>{$_('about.creators.creator_1')}</li>
+    <li>{$_('about.creators.creator_2')}</li>
+    <li>{$_('about.creators.creator_3')}</li>
+    <li>{$_('about.creators.creator_4')}</li>
   </ul>
 </div>
