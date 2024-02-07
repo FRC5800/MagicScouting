fbd91159ece1178961f1262815a83c4fff164efc
DaviFSilva <93409413+DaviFSilva@users.noreply.github.com>
2024-02-07 20:44:53 -0300

    :bug: fix(camera): fix camera error changing the uses-feature to permission

--- magicscouting/builder.bat
+++ magicscouting/builder.bat
@@ -2,5 +2,6 @@ npm run build && ^
 npx cap sync android && ^
 cd .\android\ && ^
 gradlew bundleRelease && ^
+gradlew assembleRelease && ^
 cd .\app\build\outputs\bundle\release\ && ^
 ::jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore -storepass magic123 .\app-release.aab launch_magic
