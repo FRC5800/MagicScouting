b6d06702fcf8d523ae6cbd6cccef418d0055cf3c
dl-dev02 <davi.santana@devicelogs.com>
2026-01-31 19:02:33 -0300

    update package.json and vite rules

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		allowedHosts: ["magicscouting.crabgator.com"]
+	}
 });
